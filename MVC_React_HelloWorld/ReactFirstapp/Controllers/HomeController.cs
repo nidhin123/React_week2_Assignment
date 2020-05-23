b30cc0550f258e4bc77f2c3a60eaaedf10d1235b
nidhin123 <nidhinpvm@gmail.com>
2020-05-24 00:18:33 +0530

    MVC_React_HelloWorld

---
+++ MVC_React_HelloWorld/ReactFirstapp/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReactFirstapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult getmessage()
+        {
+            return new JsonResult { Data = "Hello World. I am from server-side", JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+    }
+}
---
+++ MVC_React_HelloWorld/ReactFirstapp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReactFirstapp.MvcApplication" Language="C#" %>
