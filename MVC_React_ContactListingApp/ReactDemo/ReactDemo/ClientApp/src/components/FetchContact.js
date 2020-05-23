06e6054f9ac99d439128d2858f898e1ba634ef7d
nidhin123 <nidhinpvm@gmail.com>
2020-05-24 00:15:05 +0530

    MVCReactApp_ContactListing

---
+++ MVC_React_ContactListingApp/ReactDemo/ReactDemo/ClientApp/src/components/FetchContact.js
@@ -0,0 +1,57 @@
+﻿import React, { Component } from 'react';
+
+export class FetchContact extends Component {
+    static displayName = FetchContact.name;
+
+    constructor(props) {
+        super(props);
+        this.state = { contacts: [], loading: true };
+    }
+
+    componentDidMount() {
+        this.populateContactData();
+    }
+
+    static renderContactTable(contacts) {
+        return (
+            <table className='table table-striped' aria-labelledby="tabelLabel">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Address</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {contacts.map(contact =>
+                        <tr key={contact.name}>
+                            <td>{contact.name}</td>                          
+                            <td>{contact.address}</td>
+                            <td>{contact.email}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : FetchContact.renderContactTable(this.state.contacts);
+
+        return (
+            <div>
+                <h1 id="tabelLabel" >Contatct Listing</h1>
+                <p>This component demonstrates fetching data from the server.</p>
+                {contents}
+            </div>
+        );
+    }
+
+    async populateContactData() {
+        const response = await fetch('Contact');
+        const data = await response.json();
+        this.setState({ contacts: data, loading: false });
+    }
+}
---
+++ MVC_React_ContactListingApp/ReactDemo/ReactDemo/Controllers/ContactController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.Xml;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReactDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactController : ControllerBase
+    {
+        // GET: api/Contact
+        [HttpGet]
+        public IEnumerable<Contact> Get()
+        {
+            IList<Contact> contactlist = new List<Contact>() {
+                new Contact(){ Name="Nidhin",Address="Piravom",Email="nvarghese@suyati.com"},
+                new Contact(){ Name="Sarath",Address="Kochi",Email="sarath@suyati.com"},
+                new Contact(){ Name="Raju",Address="Aluva",Email="raju@suyati.com"},
+                new Contact(){ Name="Shibin",Address="Kannur",Email="shibin@suyati.com"}
+            };
+            return contactlist.ToArray();
+        }
+
+
+    }
+}
